6ef438442c9cbb267ab3351c1a94f4b6659b4f32
pianduan <damu@example.com>
2021-02-06 14:29:30 +0800

    完成了角色权限列表页面的功能开发

---
+++ src/components/roles/Rights.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片内容区 -->
+    <el-card>
+      <el-table :data="rightslist" style="width: 100%" border>
+        <el-table-column type="index"> </el-table-column>
+        <el-table-column prop="authName" label="权限名称"> </el-table-column>
+        <el-table-column prop="path" label="路径"> </el-table-column>
+        <el-table-column label="权限等级">
+          <template slot-scope="scope">
+            <el-tag type="primary" v-if="scope.row.level === '0'">一级</el-tag>
+            <el-tag type="success" v-else-if="scope.row.level === '1'"
+              >二级</el-tag
+            >
+            <el-tag type="danger" v-else>三级</el-tag>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 角色权限列表数据
+      rightslist: []
+    }
+  },
+  created() {
+    this.getRightList()
+  },
+  methods: {
+    async getRightList() {
+      const { data: res } = await this.$http.get('rights/list')
+      if (res.meta.status !== 200) {
+        return this.$message.error(res.meta.msg)
+      }
+      this.rightslist = res.data
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Home from '../components/Home.vue'
 import Welcome from '../components/Welcome.vue'
 import User from '../components/user/User.vue'
 import Roles from '../components/roles/Roles.vue'
+import Rights from '../components/roles/Rights.vue'
 
 Vue.use(VueRouter)
 
@@ -21,7 +22,8 @@ const router = new VueRouter({
       children: [
         { path: '/welcome', component: Welcome },
         { path: '/users', component: User },
-        { path: '/roles', component: Roles }
+        { path: '/roles', component: Roles },
+        { path: '/rights', component: Rights }
       ]
     }
   ]
